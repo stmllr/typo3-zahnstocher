d1f446e9a062f279956e7ec5a6745c66f0cb1311
Steffen Müller <typo3@t3node.com>
2016-10-05 11:00:11 +0200

    Reformat code according to PSR-2 CGL

--- Classes/Log/Processor/IntrospectionProcessor.php
+++ Classes/Log/Processor/IntrospectionProcessor.php
@@ -20,139 +20,146 @@ use TYPO3\CMS\Core\Log\LogRecord;
  * Introspection processor to automatically add where the log record came from.
  * Backported from TYPO3 7.x to support backtraces
  */
-class IntrospectionProcessor extends \TYPO3\CMS\Core\Log\Processor\AbstractProcessor {
+class IntrospectionProcessor extends \TYPO3\CMS\Core\Log\Processor\AbstractProcessor
+{
 
-	/**
-	 * Add the full backtrace to the log entry or
-	 * just the last entry of the backtrace
-	 *
-	 * @var boolean
-	 */
-	protected $appendFullBackTrace = FALSE;
+    /**
+     * Add the full backtrace to the log entry or
+     * just the last entry of the backtrace
+     *
+     * @var boolean
+     */
+    protected $appendFullBackTrace = false;
 
-	/**
-	 * Number of entries to shift from the backtrace
-	 *
-	 * @var int
-	 */
-	protected $shiftBackTraceLevel = 0;
+    /**
+     * Number of entries to shift from the backtrace
+     *
+     * @var int
+     */
+    protected $shiftBackTraceLevel = 0;
 
-	/**
-	 * Temporary storage of the preceding backtrace line number
-	 *
-	 * @var string
-	 */
-	private $precedingBacktraceLine = '';
+    /**
+     * Temporary storage of the preceding backtrace line number
+     *
+     * @var string
+     */
+    private $precedingBacktraceLine = '';
 
-	/**
-	 * Temporary storage of the preceding backtrace file
-	 *
-	 * @var string
-	 */
-	private $precedingBacktraceFile = '';
+    /**
+     * Temporary storage of the preceding backtrace file
+     *
+     * @var string
+     */
+    private $precedingBacktraceFile = '';
 
-	/**
-	 * Set the number of levels to be shift from the backtrace
-	 *
-	 * @param int $shiftBackTraceLevel Numbers of levels to shift
-	 * @return \Stmllr\Zahnstocher\Log\Processor\IntrospectionProcessor
-	 */
-	public function setShiftBackTraceLevel($shiftBackTraceLevel) {
-		$this->shiftBackTraceLevel = (int) $shiftBackTraceLevel;
-		return $this;
-	}
+    /**
+     * Set the number of levels to be shift from the backtrace
+     *
+     * @param int $shiftBackTraceLevel Numbers of levels to shift
+     * @return \Stmllr\Zahnstocher\Log\Processor\IntrospectionProcessor
+     */
+    public function setShiftBackTraceLevel($shiftBackTraceLevel)
+    {
+        $this->shiftBackTraceLevel = (int)$shiftBackTraceLevel;
 
-	/**
-	 * Set if the full backtrace should be added to the log or just the last item
-	 *
-	 * @param boolean $appendFullBackTrace If the full backtrace should be added
-	 * @return \Stmllr\Zahnstocher\Log\Processor\IntrospectionProcessor
-	 */
-	public function setAppendFullBackTrace($appendFullBackTrace) {
-		$this->appendFullBackTrace = (bool) $appendFullBackTrace;
-		return $this;
-	}
+        return $this;
+    }
 
+    /**
+     * Set if the full backtrace should be added to the log or just the last item
+     *
+     * @param boolean $appendFullBackTrace If the full backtrace should be added
+     * @return \Stmllr\Zahnstocher\Log\Processor\IntrospectionProcessor
+     */
+    public function setAppendFullBackTrace($appendFullBackTrace)
+    {
+        $this->appendFullBackTrace = (bool)$appendFullBackTrace;
 
-	/**
-	 * Add debug backtrace information to logRecord
-	 * It adds: filepath, line number, class and function name
-	 *
-	 * @param LogRecord $logRecord The log record to process
-	 * @return LogRecord The processed log record with additional data
-	 * @see debug_backtrace()
-	 */
-	public function processLogRecord(LogRecord $logRecord) {
-		$trace = $this->getDebugBacktrace();
+        return $this;
+    }
 
-		// skip TYPO3\CMS\Core\Log classes
-		foreach ($trace as $traceEntry) {
-			if (isset($traceEntry['class']) && FALSE !== strpos($traceEntry['class'], 'TYPO3\CMS\Core\Log')) {
-				$trace = $this->shiftBacktraceLevel($trace);
-			} else {
-				break;
-			}
-		}
+    /**
+     * Add debug backtrace information to logRecord
+     * It adds: filepath, line number, class and function name
+     *
+     * @param LogRecord $logRecord The log record to process
+     * @return LogRecord The processed log record with additional data
+     * @see debug_backtrace()
+     */
+    public function processLogRecord(LogRecord $logRecord)
+    {
+        $trace = $this->getDebugBacktrace();
 
-		// shift a given number of entries from the trace
-		for($i = 0; $i < $this->shiftBackTraceLevel; $i++) {
-			// shift only if afterwards there is at least one entry left after.
-			if (count($trace) > 1) {
-				$trace = $this->shiftBacktraceLevel($trace);
-			}
-		}
+        // skip TYPO3\CMS\Core\Log classes
+        foreach ($trace as $traceEntry) {
+            if (isset($traceEntry['class']) && false !== strpos($traceEntry['class'], 'TYPO3\CMS\Core\Log')) {
+                $trace = $this->shiftBacktraceLevel($trace);
+            } else {
+                break;
+            }
+        }
 
-		if ($this->appendFullBackTrace) {
-			// Add the line and file of the last entry that has these information
-			// to the first backtrace entry if it does not have this information.
-			// This is required in case we have shifted entries and the first entry
-			// is now a call_user_func that does not contain the line and file information.
-			if (!isset($trace[0]['line'])) {
-				$trace[0] = array('line' => $this->precedingBacktraceLine) + $trace[0];
-			}
-			if (!isset($trace[0]['file'])) {
-				$trace[0] = array('file' => $this->precedingBacktraceFile) + $trace[0];
-			}
+        // shift a given number of entries from the trace
+        for ($i = 0; $i < $this->shiftBackTraceLevel; $i++) {
+            // shift only if afterwards there is at least one entry left after.
+            if (count($trace) > 1) {
+                $trace = $this->shiftBacktraceLevel($trace);
+            }
+        }
 
-			$logRecord->addData(array(
-				'backtrace' => $trace
-			));
-		} else {
-			$logRecord->addData(array(
-				'file' => isset($trace[0]['file']) ? $trace[0]['file'] : NULL,
-				'line' => isset($trace[0]['line']) ? $trace[0]['line'] : NULL,
-				'class' => isset($trace[0]['class']) ? $trace[0]['class'] : NULL,
-				'function' => isset($trace[0]['function']) ? $trace[0]['function'] : NULL
-			));
-		}
+        if ($this->appendFullBackTrace) {
+            // Add the line and file of the last entry that has these information
+            // to the first backtrace entry if it does not have this information.
+            // This is required in case we have shifted entries and the first entry
+            // is now a call_user_func that does not contain the line and file information.
+            if (!isset($trace[0]['line'])) {
+                $trace[0] = array('line' => $this->precedingBacktraceLine) + $trace[0];
+            }
+            if (!isset($trace[0]['file'])) {
+                $trace[0] = array('file' => $this->precedingBacktraceFile) + $trace[0];
+            }
 
-		return $logRecord;
-	}
+            $logRecord->addData(array(
+                'backtrace' => $trace
+            ));
+        } else {
+            $logRecord->addData(array(
+                'file' => isset($trace[0]['file']) ? $trace[0]['file'] : null,
+                'line' => isset($trace[0]['line']) ? $trace[0]['line'] : null,
+                'class' => isset($trace[0]['class']) ? $trace[0]['class'] : null,
+                'function' => isset($trace[0]['function']) ? $trace[0]['function'] : null
+            ));
+        }
 
-	/**
-	 * Shift the first item from the backtrace
-	 *
-	 * @param array $backtrace
-	 * @return array
-	 */
-	protected function shiftBacktraceLevel(array $backtrace) {
-		if (isset($backtrace[0]['file'])) {
-			$this->precedingBacktraceFile = $backtrace[0]['file'];
-		}
-		if (isset($backtrace[0]['line'])) {
-			$this->precedingBacktraceLine = $backtrace[0]['line'];
-		}
-		array_shift($backtrace);
+        return $logRecord;
+    }
 
-		return $backtrace;
-	}
+    /**
+     * Shift the first item from the backtrace
+     *
+     * @param array $backtrace
+     * @return array
+     */
+    protected function shiftBacktraceLevel(array $backtrace)
+    {
+        if (isset($backtrace[0]['file'])) {
+            $this->precedingBacktraceFile = $backtrace[0]['file'];
+        }
+        if (isset($backtrace[0]['line'])) {
+            $this->precedingBacktraceLine = $backtrace[0]['line'];
+        }
+        array_shift($backtrace);
 
-	/**
-	 * Get the debug backtrace
-	 *
-	 * @return array
-	 */
-	protected function getDebugBacktrace() {
-		return debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-	}
+        return $backtrace;
+    }
+
+    /**
+     * Get the debug backtrace
+     *
+     * @return array
+     */
+    protected function getDebugBacktrace()
+    {
+        return debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    }
 }
--- Classes/Log/Writer/FileWriter.php
+++ Classes/Log/Writer/FileWriter.php
@@ -22,43 +22,45 @@ use TYPO3\CMS\Core\Log\Writer\WriterInterface;
  * FileWriter which extends the core FileWriter,
  * using print_r instead of json to format its data.
  */
-class FileWriter extends \TYPO3\CMS\Core\Log\Writer\FileWriter {
+class FileWriter extends \TYPO3\CMS\Core\Log\Writer\FileWriter
+{
 
-	/**
-	 * Writes the log record
-	 *
-	 * @param LogRecord $record Log record
-	 * @return WriterInterface $this
-	 * @throws \RuntimeException
-	 */
-	public function writeLog(LogRecord $record) {
-		$timestamp = date('r', (int)$record->getCreated());
-		$levelName = LogLevel::getName($record->getLevel());
-		$data = '';
-		$recordData = $record->getData();
-		if (!empty($recordData)) {
-			// According to PSR3 the exception-key may hold an \Exception
-			// Since json_encode() does not encode an exception, we run the _toString() here
-			if (isset($recordData['exception']) && $recordData['exception'] instanceof \Exception) {
-				$recordData['exception'] = (string)$recordData['exception'];
-			}
-			$data = '- ' . print_r($recordData, TRUE);
-		}
+    /**
+     * Writes the log record
+     *
+     * @param LogRecord $record Log record
+     * @return WriterInterface $this
+     * @throws \RuntimeException
+     */
+    public function writeLog(LogRecord $record)
+    {
+        $timestamp = date('r', (int)$record->getCreated());
+        $levelName = LogLevel::getName($record->getLevel());
+        $data = '';
+        $recordData = $record->getData();
+        if (!empty($recordData)) {
+            // According to PSR3 the exception-key may hold an \Exception
+            // Since json_encode() does not encode an exception, we run the _toString() here
+            if (isset($recordData['exception']) && $recordData['exception'] instanceof \Exception) {
+                $recordData['exception'] = (string)$recordData['exception'];
+            }
+            $data = '- ' . print_r($recordData, true);
+        }
 
-		$message = sprintf(
-			'%s [%s] request="%s" component="%s": %s %s',
-			$timestamp,
-			$levelName,
-			$record->getRequestId(),
-			$record->getComponent(),
-			$record->getMessage(),
-			$data
-		);
+        $message = sprintf(
+            '%s [%s] request="%s" component="%s": %s %s',
+            $timestamp,
+            $levelName,
+            $record->getRequestId(),
+            $record->getComponent(),
+            $record->getMessage(),
+            $data
+        );
 
-		if (FALSE === fwrite(self::$logFileHandles[$this->logFile], $message . LF)) {
-			throw new \RuntimeException('Could not write log record to log file', 1345036335);
-		}
+        if (false === fwrite(self::$logFileHandles[$this->logFile], $message . LF)) {
+            throw new \RuntimeException('Could not write log record to log file', 1345036335);
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 }
